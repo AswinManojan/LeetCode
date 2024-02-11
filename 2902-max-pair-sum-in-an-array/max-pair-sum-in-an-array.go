87adeafc373a2463bf27017393bf070ef51883fe
Aswin M <125151196+AswinManojan@users.noreply.github.com>
2024-02-11 21:19:42 +0530

    Time: 19 ms (56.32%) | Memory: 4.6 MB (88.51%) - LeetSync

--- 2902-max-pair-sum-in-an-array/max-pair-sum-in-an-array.go
+++ 2902-max-pair-sum-in-an-array/max-pair-sum-in-an-array.go
@@ -1,14 +1,12 @@
 func maxSum(nums []int) int {
     max:=-1
-    sum:=-1
     for i:=0;i<len(nums);i++{
         mi:=maxDigit(nums[i])
         for j:=i+1;j<len(nums);j++{
             if mi==maxDigit(nums[j]){
-                sum=nums[i]+nums[j]
-            }
-            if sum>max{
-                max=sum
+                if max<(nums[i]+nums[j]){
+                    max=nums[i]+nums[j]
+                }
             }
         }
     }
