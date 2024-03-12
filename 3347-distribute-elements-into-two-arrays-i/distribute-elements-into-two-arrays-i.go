a30858ea0327fd4e21ceeade18372be9eb3a23a2
Aswin M <125151196+AswinManojan@users.noreply.github.com>
2024-03-12 10:44:52 +0530

    Time: 3 ms (69.64%) | Memory: 3.1 MB (29.76%) - LeetSync

--- 3347-distribute-elements-into-two-arrays-i/distribute-elements-into-two-arrays-i.go
+++ 3347-distribute-elements-into-two-arrays-i/distribute-elements-into-two-arrays-i.go
@@ -1,7 +1,6 @@
 func resultArray(nums []int) []int {
-    var arr1,arr2 []int
-    arr1=append(arr1,nums[0])
-    arr2=append(arr2,nums[1])
+    arr1:=[]int{nums[0]}
+    arr2:=[]int{nums[1]}
     for i:=2;i<len(nums);i++{
         if arr1[len(arr1)-1]>arr2[len(arr2)-1]{
             arr1=append(arr1,nums[i])
